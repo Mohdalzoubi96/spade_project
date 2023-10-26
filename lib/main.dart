af966f64bbedb96a3887d9cdcc8d99ed9ca46130
AhmedAmmous <a.ammous1997@outlook.com>
2023-10-27 00:13:20 +0300

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async{
-
+//test comment
   runZonedGuarded<Future<void>>(
         () async {
       WidgetsFlutterBinding.ensureInitialized();
